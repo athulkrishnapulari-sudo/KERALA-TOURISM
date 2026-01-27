a094eed39c5b799da4fcc7a8c9d853c52d76dd9b
athulkrishnapulari-sudo <athulkrishnapulari@gmail.com>
2026-01-27 18:24:24 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,81 @@
+const btn=document.getElementById("nav_btn");
+const box=document.querySelector(".links");
+const icon=document.getElementById("ham_icon")
+
+btn.addEventListener('click',()=>{
+    if (links.classList.contains('show')){
+        links.classList.remove('show');
+        icon.style.transform='rotate(0deg)';
+        
+    }else{
+        links.classList.add('show');
+        icon.style.transform='rotate(90deg)';
+    }
+});
+
+const item5=document.getElementById('nav_item5');
+
+
+
+const item1=document.getElementById('nav_item1');
+
+item1.addEventListener('click',()=>{
+    if (links.classList.contains('show')){
+        links.classList.remove('show');
+        icon.style.transform='rotate(0deg)';
+        
+    }else{
+        links.classList.add('show');
+        icon.style.transform='rotate(90deg)';
+    }
+})
+
+const item2=document.getElementById('nav_item2');
+
+item2.addEventListener('click',()=>{
+    if (links.classList.contains('show')){
+        links.classList.remove('show');
+        icon.style.transform='rotate(0deg)';
+        
+    }else{
+        links.classList.add('show');
+        icon.style.transform='rotate(90deg)';
+    }
+})
+
+const item3=document.getElementById('nav_item3');
+
+item3.addEventListener('click',()=>{
+    if (links.classList.contains('show')){
+        links.classList.remove('show');
+        icon.style.transform='rotate(0deg)';
+        
+    }else{
+        links.classList.add('show');
+        icon.style.transform='rotate(90deg)';
+    }
+})
+item5.addEventListener('click',()=>{
+    if (links.classList.contains('show')){
+        links.classList.remove('show');
+        icon.style.transform='rotate(0deg)';
+        
+    }else{
+        links.classList.add('show');
+        icon.style.transform='rotate(90deg)';
+    }
+})
+
+
+const item4=document.getElementById('nav_item4');
+
+item4.addEventListener('click',()=>{
+    if (links.classList.contains('show')){
+        links.classList.remove('show');
+        icon.style.transform='rotate(0deg)';
+        
+    }else{
+        links.classList.add('show');
+        icon.style.transform='rotate(90deg)';
+    }
+})
